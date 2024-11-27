7ea4f90477ba64d8942da5c3e7ddb4dfdcfbf639
Faithful Eli Coronel <faithful.e.v.coronel@outlook.com>
2024-11-28 00:13:19 +0800

    Fixed TestimonialSection responsiveness

--- src/components/mra/TestimonialSection.tsx
+++ src/components/mra/TestimonialSection.tsx
@@ -63,7 +63,7 @@ const TestimonialSection: React.FC = () => {
       <div className="max-w-7xl mx-auto">
         {/* Header and Testimonials */}
         <div className="flex flex-col lg:flex-row justify-between">
-          <h2 className="text-3xl pb-10 font-regular text-left lg:w-1/4">
+          <h2 className="text-3xl pb-10 ml-3 lg:ml-16 font-regular text-left lg:w-1/4">
             What our clients are saying about us?
           </h2>
 
